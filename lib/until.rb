c398e6a1b05f118e122606748ca426efa6758207
celebrated-integer-2023 <scailles23@gmail.com>
2019-08-29 05:45:06 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,7 @@
 def using_until do
   levitation_force=6
-  loop do
-  levitation_force+=1
-  until levitation_force=10 
   puts "Wingardium Leviosa"
+   levitation_force+=1
+  until levitation_force=10
 end
 
